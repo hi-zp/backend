3b4a0e6470f5beb46236539d1c1abce3481c156c
hi-zp <987107833@qq.com>
2024-04-02 11:56:35 +0800

    feat: prisma for db, rest with swagger, mercurius for graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { CatsModule } from './cats/cats.module';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [CoreModule, CatsModule],
+  imports: [
+    CoreModule,
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      autoSchemaFile: 'schema.gql',
+      subscription: true,
+      graphiql: true,
+    }),
+    RecipesModule,
+    CatsModule,
+    UserModule,
+    PostModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,22 @@ async function bootstrap() {
   app.register(helmet, {
     contentSecurityPolicy: {
       directives: {
-        defaultSrc: [`'self'`],
-        styleSrc: [`'self'`, `'unsafe-inline'`],
-        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
-        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+        defaultSrc: [`'self'`, 'unpkg.com'],
+        styleSrc: [
+          `'self'`,
+          `'unsafe-inline'`,
+          'cdn.jsdelivr.net',
+          'fonts.googleapis.com',
+          'unpkg.com',
+        ],
+        fontSrc: [`'self'`, 'fonts.gstatic.com', 'data:'],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io', 'cdn.jsdelivr.net'],
+        scriptSrc: [
+          `'self'`,
+          `https: 'unsafe-inline'`,
+          `cdn.jsdelivr.net`,
+          `'unsafe-eval'`,
+        ],
       },
     },
   });
